0ee4982935428c0aed15e16484d1820a9585fb8f
Samarann <88876@farnborough.ac.uk>
2019-12-13 14:31:41 +0000

    doing html now

---
+++ resources/client/js/main.js
@@ -0,0 +1,19 @@
+function pageLoad(){
+    //let now = new Date();
+
+    //let myHTML = '<div style="text-align: center;">' +
+    //    '<h1>Welcome to my API powered website!</h1>' +
+    //    '<img src="/client/img/logo.png" alt="Logo"/>' +
+    //    '<div style="front-style: italic;">' +
+    //    'Generated at ' + now.toLocaleTimeString() +
+    //    '</div>' +
+    //    '</div>';
+
+   //     document.getElementById("testDiv").innerHTML = myHTML;
+
+    let HTML = '<table>' +
+        '<tr>' +
+        '<th>ID</th>' +
+        '<th>Name</th>' +
+        '</tr>';
+}
--- src/main/java/Controllers/UserArea.java
+++ src/main/java/Controllers/UserArea.java
@@ -3,12 +3,20 @@ package Controllers;
 import Server.Main;
 import org.json.simple.JSONArray;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+@Path("folders/")
 public class UserArea {
+    @GET
+    @Path("readall/")
+    @Produces(MediaType.APPLICATION_JSON)
     public static void readUserArea(){
-        System.out.println("folders/read/");
+        System.out.println("folders/readall/");
         JSONArray read = new JSONArray();
         try{
             PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM UserArea");
@@ -21,6 +29,7 @@ public class UserArea {
             //return read.toString();
         }catch (Exception e){
             System.out.println("Database error: " + e.getMessage());
+            //return "{\"error\"; \"Unable to list items, please see server console for more info.\"}";
         }
     }
 }
