266d833971b9f940ee2910fdda0885d2ded2b5f6
Samarann <88876@farnborough.ac.uk>
2020-01-13 18:00:25 +0000

    css still not working what the hell

--- resources/client/js/main.js
+++ resources/client/js/main.js
@@ -1,79 +1,115 @@
 function pageLoad(){
-
-    let myHTML = '<div style="text-align: center;">' +
-        '<h1>Welcome to my API powered website!</h1>' +
-        '<img src="/client/img/logo.png" alt="Logo"/>' +
-        '<div style="front-style: italic;">' +
-        'Generated at ' + now.toLocaleTimeString() +
-        '</div>' +
-        '</div>';
-
-    document.getElementById("testDiv").innerHTML = myHTML;
-
-        let HTML = '<table>' +
+    let savesHTML = '<table>' +
         '<tr>' +
         '<th>ID</th>' +
-        '<th>Name</th>' +
-        '</tr>';
+        '<th>Save Name</th>' +
+        '<th class="last">Options</th>' +
+        '</tr>'
 
-        fetch('/client/main', {method: 'get'}).then(response => response.json()).then
+        fetch('/saves/read', {method: 'get'}).then(response => response.json()).then( saves => {
 
-    if(window.location.search === '?logout') {
-        document.getElementById('content').innerHTML = '<h1>Logging out, please wait...</h1>';
-        logout();
+            for(let read of saves){
+                savesHTML += `<tr>` +
+                    `<td>${saves.SaveID}</td>` +
+                    `<td>${saves.SaveName}</td>` +
+                    `<td class="last">` +
+                    `<button class='editButton' data-id='${saves.SaveID}'>Edit</button>button` +
+                    `<button class='deleteButton' data-id='${saves.SaveID}'>Delete</button>button` +
+                    `</td>`
+                    `</tr>`;
+            }
+
+            savesHTML += `</table>`;
+
+            document.getElementById("listDiv").innerHTML = savesHTML;
+
+            let editButtons = document.getElementsByClassName("editButton");
+            for (let button of editButtons){
+                button.addEventListener("click", editSaves);
+            }
+
+            let deleteButtons = document.getElementsByClassName("deleteButton");
+            for (let button of deletebuttons){
+                button.addEventListener("click", deleteSaves)
+            }
+    });
+
+    document.getElementById("saveButton").addEventListener("click", saveEditSaves);
+    document.getElementById("cancelButton").addEventListener("click", cancelEditSaves);
+}
+
+function editSaves(event){
+    const id = event.target.getAttribute("data-id");
+
+    if(id === null){
+        document.getElementById("editHeading").innerHTML = 'Add new fruit:';
+
+        document.getElementById("SaveID").value = '';
+        document.getElementById("SaveName").value = '';
+
+        document.getElementById("listDiv").style.display = 'none';
+        document.getElementById("editDiv").style.display = 'block';
     } else {
-        document.getElementById("loginButton").addEventListener("click", login);
-    }
-
-    function login(event) {
-
-        event.preventDefault();
-
-        const form = document.getElementById("loginForm");
-        const formData = new FormData(form);
-
-        fetch("/users/login", {method: 'post', body: formData}
-        ).then(response => response.json()
-        ).then(responseData => {
-
-            if (responseData.hasOwnProperty('error')) {
-                alert(responseData.error);
+        fetch('saves/read/' + id, {method: 'get'}).then(response => response.json()).then(fruit =>{
+            if(saves.hasOwnProperty('error')){
+                alert(save.error);
             } else {
-                Cookies.set("username", responseData.username);
-                Cookies.set("token", responseData.token);
+                document.getElementById("editHeading").innerHTML = 'Editing ' + save.SaveName + ':';
 
-                window.location.href = '../main.html';
+                document.getElementById("SaveID").value = id;
+                document.getElementById("SaveName").value = save.SaveName;
+
+                document.getElementById("listDiv").style.display = 'none';
+                document.getElementById("editDiv").style.display = 'block';
             }
         });
     }
+}
 
-    fetch("/users/logout", {method: 'post'}
-    ).then(response => response.json()
-    ).then(responseData => {
-        if (responseData.hasOwnProperty('error')) {
+function saveEditSaves(event){
+    event.preventDefault();
 
+    if(document.getElementById("SaveName").value.trim() === ''){
+        alert("Please provide a save name.");
+        return;
+    }
+
+    if(document.getElementById("SaveOwner").value.trim() === ''){
+        alert("Please provide a save name.");
+        return;
+    }
+
+    const id = document.getElementById("SaveID").value;
+    const form = document.getElementById("SaveForm");
+    const formData = new FormData(form);
+
+    let apiPath = '';
+    if (id === ''){
+        apiPath = 'saves/create';
+    } else {
+        apiPath = 'saves/update';
+    }
+
+    fetch(apiPath, {method: 'post', body: formData}).then response => response.json()).then(responseData => {
+        if(responseData.hasOwnProperty('error')){
             alert(responseData.error);
-
         } else {
-
-            Cookies.remove("username");
-            Cookies.remove("token");
-
-            window.location.href = '../main.html';
-
+            document.getElementById("listDiv").style.display = 'block';
+            document.getElementById("editDiv").style.display = 'none';
         }
     });
+}
 
+function cancelEditSaves(event){
+    event.preventDefault();
 
-    //let now = new Date();
+    document.getElementById("listDiv").style.display = 'block';
+    document.getElementById("editDiv").style.display = 'none';
+}
 
-    //let myHTML = '<div style="text-align: center;">' +
-    //    '<h1>Welcome to my API powered website!</h1>' +
-    //    '<img src="/client/img/logo.png" alt="Logo"/>' +
-    //    '<div style="front-style: italic;">' +
-    //    'Generated at ' + now.toLocaleTimeString() +
-    //    '</div>' +
-    //    '</div>';
+function deleteSaves(event){
+    const confirmation = confirm("Are you sure you want to delete this save?");
 
-   //     document.getElementById("testDiv").innerHTML = myHTML;
+    if (confirmation === true){
+        let id = event.target.getAttribute("data=id);
 }
